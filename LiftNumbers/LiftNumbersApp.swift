06133419ba3a9081cfc4a51b13bc2624821bd633
domlord <dominicrlord@gmail.com>
2026-05-05 19:23:37 +0100

    Initial Commit

---
+++ LiftNumbers/LiftNumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiftNumbersApp.swift
+//  LiftNumbers
+//
+//  Created by Dominic Lord on 05/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct LiftNumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
