3ce43df8130190a8c34bf57339d39d933000b857
domlord <dominicrlord@gmail.com>
2026-05-11 15:43:34 +0100

    content view

--- LiftNumbers/App/ContentView.swift
+++ LiftNumbers/App/ContentView.swift
@@ -8,19 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var authViewModel = AuthViewModel()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        if authViewModel.isLoggedIn {
+            Text("Main App")
         }
-        .padding()
+        else {
+            Text("Login")
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
