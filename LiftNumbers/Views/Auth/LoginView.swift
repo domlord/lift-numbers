f8078ea738fb120ab5164c0fa28ad482a351931c
domlord <dominicrlord@gmail.com>
2026-05-12 20:59:34 +0100

    start sign up and code login

--- LiftNumbers/Views/Auth/LoginView.swift
+++ LiftNumbers/Views/Auth/LoginView.swift
@@ -5,4 +5,52 @@
 //  Created by Dominic Lord on 11/05/2026.
 //
 
-import Foundation
+import SwiftUI
+
+struct LoginView : View {
+    @EnvironmentObject var authViewModel: AuthViewModel
+
+    @State private var email = ""
+    @State private var password = ""
+    
+    var body: some View {
+          VStack(spacing: 20) {
+
+              Text("LiftNumbers")
+                  .font(.largeTitle)
+                  .bold()
+
+              TextField("Email", text: $email)
+                  .textFieldStyle(RoundedBorderTextFieldStyle())
+                  .autocapitalization(.none)
+                  .keyboardType(.emailAddress)
+
+              SecureField("Password", text: $password)
+                  .textFieldStyle(RoundedBorderTextFieldStyle())
+
+              if !authViewModel.errorMessage.isEmpty {
+                  Text(authViewModel.errorMessage)
+                      .foregroundColor(.red)
+                      .font(.caption)
+              }
+
+              if authViewModel.isLoading {
+                  ProgressView()
+              } else {
+                  Button("Log In") {
+                      Task {
+                          await authViewModel.logIn(email: email, password: password)
+                      }
+                  }
+                  .buttonStyle(.borderedProminent)
+              }
+
+              NavigationLink("Don't have an account? Sign up here") {
+                  SignUpView()
+              }
+
+          }
+          .padding()
+      }
+  }
+
--- LiftNumbers/Views/Auth/SignUpView.swift
+++ LiftNumbers/Views/Auth/SignUpView.swift
@@ -5,4 +5,21 @@
 //  Created by Dominic Lord on 11/05/2026.
 //
 
-import Foundation
+import SwiftUI
+
+struct SignUpView : View {
+    
+    @EnvironmentObject var authViewModel : AuthViewModel
+    
+    @State private var email = ""
+    @State private var password = ""
+    @State private var confirmPassword = ""
+    
+    var body : some View {
+        VStack(spacing: 20){
+            
+            Text
+        }
+    }
+    
+}
