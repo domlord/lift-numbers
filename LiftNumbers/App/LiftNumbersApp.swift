c1f109791c296dddbc3dea02f1b3c5e2b60d7560
domlord <dominicrlord@gmail.com>
2026-05-29 01:20:47 +0100

    add stateobject authviewmodel

--- LiftNumbers/App/LiftNumbersApp.swift
+++ LiftNumbers/App/LiftNumbersApp.swift
@@ -4,14 +4,23 @@
 //
 //  Created by Dominic Lord on 05/05/2026.
 //
-
+import FirebaseCore
 import SwiftUI
+
 
 @main
 struct LiftNumbersApp: App {
+    
+    @StateObject var authViewModel = AuthViewModel()
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(authViewModel)
         }
     }
 }
